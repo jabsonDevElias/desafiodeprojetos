3f3ad5c2bb1c0c5d1587d94f891eefd12acbfae6
jabsonDevElias <jabsonelias@gmail.com>
2024-09-25 20:51:04 -0300

    feat: Finalizado o projeto.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from 'react'
 import "./Home.scss";
 import axios from 'axios';
 
+var Array:any = [];
 
 interface CardProps {
   titulo: string; 
   numero: string;
+  dados: any;
 }
 
 function getRandomElement(arr:any) {
@@ -13,30 +15,45 @@ function getRandomElement(arr:any) {
     return arr[randomIndex];
 }
 
-const Card: React.FC<CardProps> = ({ titulo,numero }) => {
+
+const Card: React.FC<CardProps> = ({ titulo,numero,dados}) => {
 
   const [tecnologias,setTecnologias] = useState<any>([]);
   const [status,setStatus] = useState<any>(0);
 
+
   useEffect(() => {   
-    // return () => {
+    return () => {
         axios.get(`http://localhost:9999/tecnologias/${numero}`)
         .then(function (response) {
-          setTecnologias([getRandomElement(response.data)]);
+          const dados_aleatorios = [getRandomElement(response.data)];
+            setTecnologias(dados_aleatorios);
+            dados((prevItens:any) => [...prevItens, dados_aleatorios[0]]);
+          // dados_aleatorios.map((item:any) => {
+          //   Array.push(item.id);
+          // })
           
+          // dados(dados_aleatorios);      
           // setKeyAleatoria(Math.floor(Math.random() * tecnologias.length));
         })
         .catch(function (error) {
           console.error(error);
         })
         .finally(function () {
-          setTimeout(() => {
+          
+          // setTimeout(() => {
             setStatus(1);
-          }, 3000);
+  
+          // }, 3000);
              
         });
-    // };
+
+
+        
+    };
   }, []);
+
+ 
 
   // if(!status){
   //   return(
@@ -45,11 +62,11 @@ const Card: React.FC<CardProps> = ({ titulo,numero }) => {
   // }
 
   return (
-  
     <div className='col-5 col-md-2 text-center'>
+
       <h3>{titulo}</h3>
       <div className="d-flex card-tecnologia col-12 rounded-3 shadow-sm">
-        {tecnologias.map((item:any,key:any) => <div className='d-flex align-items-center flex-wrap m-auto justify-content-between'><img className="col-4 m-auto" src={`assets/${item.id}.png`} alt="" /><p className='fw-bolder col-12'>{item.tecn_tx_nome}</p></div>)}
+        {tecnologias.map((item:any,key:any) => <div className='d-flex align-items-center flex-wrap m-auto justify-content-between' key={key}><img className="col-4 m-auto" src={`assets/${item.id}.png`} alt="" /><p className='fw-bolder col-12'>{item.tecn_tx_nome}</p></div>)}
       </div>
     </div>
   );
@@ -59,11 +76,10 @@ const Card: React.FC<CardProps> = ({ titulo,numero }) => {
 const Home = () => {
 
     const [categorias,setCategorias] = useState<any>([]);
-
-    // useEffect(() => {   
-    //   return () => {
+    const [dadossorteio,setDadosSorteio] = useState<any>([]);
+    
     function sortear(){
-      setCategorias([]);
+          setCategorias([]);
           axios.get('http://localhost:9999/categorias/')
           .then(function (response) {
             setCategorias(response.data);
@@ -75,14 +91,13 @@ const Home = () => {
             
           });
     }
-
-
     useEffect(() => {   
-      return () => {
+      return () => {     
        sortear();
-              };
+      };
     }, []);
 
+    
   return (
       <>
         <div className="col-12 d-flex flex-wrap justify-content-between container">
@@ -90,43 +105,21 @@ const Home = () => {
           <div className="col-12 text-center mb-5">
               <h1>Escolhedor de Tecnologias</h1>
           </div>
-          {categorias.map((item:any) => 
-            <Card titulo={item.cate_tx_nome} numero={item.id} key={item.id}/>
+          {/* {
+  Array.map((item:any) => <h3>{item}</h3>)
+} */}
+
+          {categorias.map((item:any,key:any) => 
+            <Card titulo={item.cate_tx_nome} numero={item.id} key={key} dados={setDadosSorteio}/>
           )}
 
+{/* {
+  dadossorteio.map((item:any) => <><h1>{item.tecn_tx_nome}</h1><br/></>)
+} */}
          <div className="col-12">
-            <div className="d-flex col-4 mt-5 mb-3 text-center justify-content-between">
+            <div className="d-flex col-12 mt-5 mb-3 text-center">
                 <button className="btn btn-warning" onClick={()=>sortear()}>SORTEAR TECNOLOGIAS</button>
-                <button className="btn btn-success" onClick={()=>sortear()}>CADASTRAR SORTEIO</button>
             </div>
-          </div>
-
-          <div className="col-12 mt-5 ">
-              <table className="table table-dark table-custom">
-                <thead>
-                  <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Nome do Projeto</th>
-                    {categorias.map((item:any) => 
-                        <th scope="col">{item.cate_tx_nome}</th>
-                    )}
-                  </tr>
-                </thead>
-                <tbody>
-                  {/* <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                  </tr> */}
-                </tbody>
-              </table>
           </div>
         </div>
       </>
